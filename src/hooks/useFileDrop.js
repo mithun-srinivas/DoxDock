d4c58965cdf69d2a1ef42617872dc5aa26ccb30f
Debdip Bhattacharya <dbhattacharya1912@gmail.com>
2026-07-13 22:24:59 +0530

    whole window drag and drop feature done

---
+++ src/hooks/useFileDrop.js
@@ -0,0 +1,10 @@
+import { useEffect } from "react";
+import { registerFileDropHandler } from "../lib/fileDropBus";
+
+
+export function useFileDrop(onFile){
+useEffect(()=>{
+    const unregister = registerFileDropHandler(onFile)
+    return unregister;
+}, [onFile])
+}
