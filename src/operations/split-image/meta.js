d03c1f520b5c118733d0c9bab5e1b5aa1de8ee30
Pawan Kumar <pawankumarg881156@gmail.com>
2026-08-19 11:08:01 +0530

    feat: add image splitter

---
+++ src/operations/split-image/meta.js
@@ -0,0 +1,8 @@
+export default {
+  id: 'split-image',
+  name: 'Split Image',
+  description: 'Split an image into a grid of tiles and download them as a ZIP.',
+  category: 'image',
+  icon: 'grid',
+  order: 42,
+}
